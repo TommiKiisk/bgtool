1742491f8a04da15bf62a75cea6d543de0ff1054
toom-kaarel.kiisk@myy.haaga-helia.fi <toom-kaarel.kiisk@myy.haaga-helia.fi>
2025-11-22 16:57:30 +0200

    projektin jäsentelyä ja navigaatio buttoneilla homescreenilla

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
-import { NavigationContainer } from '@react-navigation/native';
+import { createStaticNavigation, NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from './components/HomeScreen'
-import SettingScreen from './components/TimerScreen'
-import Dice from './components/Dice';
+import TimerScreen from './components/TimerScreen'
+import DiceScreen from './components/Dice';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { View, Text, Button } from 'react-native';
 
 
 const Tab = createBottomTabNavigator();
+
+
+
+function HomeScreen() {
+  const navigation = useNavigation<any>();
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Welcome to Boardgametool</Text>
+      <Button title="Timer" onPress={() => navigation.navigate('Timer')}/>
+      <Button title="Dice" onPress={() => navigation.navigate('Dice')}/>
+    </View>
+  );
+}
+
 
   
 export default function App() {
@@ -13,9 +29,20 @@ export default function App() {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Timer" component={SettingScreen} />
-        <Tab.Screen name="Dice" component={Dice} />
+        <Tab.Screen name="Timer" component={TimerScreen} />
+        <Tab.Screen name="Dice" component={DiceScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
+
+const RootStack = createNativeStackNavigator({
+  initialRouteName: 'Home',
+  screens: {
+    Home: HomeScreen,
+    Timer: TimerScreen,
+    Dice: DiceScreen,
+  },
+});
+
+const Navigation = createStaticNavigation(RootStack);
